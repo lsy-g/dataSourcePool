ad6d181ac50247e6aa2cd67d4e8e6ef505c6e80f
lsy-g <974014377@qq.com>
2021-10-25 16:00:33 +0800

    gitignore

---
+++ src/main/java/pool/ConnPool.java
@@ -0,0 +1,131 @@
+package pool;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.SQLFeatureNotSupportedException;
+import java.util.LinkedList;
+import java.util.Properties;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.logging.Logger;
+
+import javax.sql.DataSource;
+
+/**
+ * 简单实现数据库连接池
+ *
+ */
+public class ConnPool implements DataSource {
+
+    //使用LinkedList集合存放数据库连接
+    private static LinkedList<Connection> connPool = new LinkedList<Connection>();
+    // 连接总数
+    private static String driver;
+    private static String url;
+    private static String user;
+    private static String password;
+
+    private static int maxSize;
+    private static int survivalTime;
+
+    private static AtomicInteger totalSize= new AtomicInteger(0);;
+
+    //在静态代码块中加载配置文件
+    static{
+        InputStream in = ConnPool.class.getClassLoader().getResourceAsStream("db.properties");
+        Properties prop = new Properties();
+        try {
+            prop.load(in);
+            driver = prop.getProperty("driver");
+            url = prop.getProperty("url");
+            user = prop.getProperty("user");
+            password = prop.getProperty("password");
+            //数据库连接池的初始化连接数的大小
+            int  InitSize = Integer.parseInt(prop.getProperty("InitSize"));
+            // 最大连接
+            maxSize = Integer.parseInt(prop.getProperty("maxSize"));
+            // 存活时间
+            survivalTime = Integer.parseInt(prop.getProperty("survivalTime"));
+            //加载驱动
+            Class.forName(driver);
+            for(int loop=0; loop<InitSize; loop++){
+                Connection conn = DriverManager.getConnection(url, user, password);
+                System.out.println("初始化数据库连接池，创建第 " + (loop + 1) +" 个连接，添加到池中");
+                connPool.add(conn);
+                totalSize.incrementAndGet();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    /**
+     * 获取数据库连接
+     */
+    public Connection getConnection() throws SQLException {
+        if(connPool.size() > 0){
+            //从集合中获取一个连接
+            final Connection conn = connPool.removeFirst();
+            //返回Connection的代理对象
+            return conn;
+        }else{
+            if (totalSize.intValue()<maxSize){
+                // 添加连接
+                Connection conn = DriverManager.getConnection(url, user, password);
+//                connPool.add(conn);
+                totalSize.incrementAndGet();
+                return conn;
+            }else {
+                // 连接到上限
+                throw new RuntimeException("数据库繁忙，稍后再试");
+            }
+        }
+    }
+
+    public PrintWriter getLogWriter() throws SQLException {
+        return null;
+    }
+
+    public void setLogWriter(PrintWriter out) throws SQLException {
+
+    }
+
+    public void setLoginTimeout(int seconds) throws SQLException {
+
+    }
+
+    public int getLoginTimeout() throws SQLException {
+        return 0;
+    }
+
+    public Logger getParentLogger() throws SQLFeatureNotSupportedException {
+        return null;
+    }
+
+    public Object unwrap(Class iface) throws SQLException {
+        return null;
+    }
+
+    public boolean isWrapperFor(Class iface) throws SQLException {
+        return false;
+    }
+
+    public Connection getConnection(String username, String password)
+            throws SQLException {
+        return null;
+    }
+
+
+}
+
---
+++ src/test/java/PoolTest.java
@@ -0,0 +1,28 @@
+import util.JdbcUtil;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+
+public class PoolTest {
+
+    /**
+     * 测试数据库连接池
+     * @param args
+     */
+    @SuppressWarnings("all")
+    public static void main(String[] args) {
+        JdbcUtil util = new JdbcUtil();
+        try {
+            Connection conn = util.getConnection();
+            if(conn != null){
+                System.out.println("我得到了一个连接");
+            }
+            util.CloseConnection(conn, null, null);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
